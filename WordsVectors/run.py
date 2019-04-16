6e27b010c28d15bea315c8543a6e356fcf78ee10
Ruthvik <engg.ruthvik@gmail.com>
2019-04-16 17:05:09 +0530

    Add saver object and Tensorboard projector.

--- WordsVectors/run.py
+++ WordsVectors/run.py
@@ -1,4 +1,5 @@
 # import libraries
+import os
 import pickle
 import numpy as np
 import tensorflow as tf
@@ -23,12 +24,20 @@ with graph.as_default() as g:
 
     model = SkipGram(vocab_size=vocab_size, embedding_dim=128, window_size=window_size, batch_size=16, graph=g)
 
-    with tf.Session() as sess:
+    # configure GPU options
+    sess_config = tf.ConfigProto(allow_soft_placement=True)
+    sess_config.gpu_options.allow_growth = True
+    
+    with tf.Session(config=sess_config) as sess:
 
         writer = tf.summary.FileWriter("./logs")
         sess.run(tf.global_variables_initializer())
         saver = tf.train.Saver()
 
+        if os.path.exists(os.path.join("./save_dir", "checkpoint")):
+            saver.restore(sess, tf.train.latest_checkpoint("./save_dir"))
+            print("Writing Embeddings to a file...")
+            np.save("./embeddings.npy", model.embeddings.eval())
         global_step = max(sess.run(model.global_step), 1)
 
         for _ in range(global_step, 150000):
@@ -42,4 +51,10 @@ with graph.as_default() as g:
             loss_sum = tf.Summary(value=[tf.Summary.Value(tag="model/loss", simple_value=loss), ])
             writer.add_summary(loss_sum, global_step)
 
-            print("Loss at step {} - {}".format(_, loss))
+            if global_step % 1000 == 0:
+                print("Loss at step {} - {}".format(_, loss))
+
+            if global_step % 50000 == 0:
+                print("Saving checkpoint..")
+                filename = os.path.join("./save_dir", "model_{}".format(global_step))
+                saver.save(sess, filename)
